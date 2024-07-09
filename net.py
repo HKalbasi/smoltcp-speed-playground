838aaf34222c01d4c9e9dcc8452ba630f42928a8
hkalbasi <hamidrezakalbasi@protonmail.com>
2024-07-10 01:40:22 +0330

    Initial commit

---
+++ net.py
@@ -0,0 +1,71 @@
+"""
+Host in the middle Mininet setup
+┌───────────────┐     ┌───────────────┐      ┌───────────────┐
+│               │     │               │      │               │
+│               │     │               │      │               │
+│   Left Host  ◄├─────┤► Middle Host ◄├──────┤►  Right Host  │
+│               │     │               │      │               │
+│               │     │               │      │               │
+└───────────────┘     └───────────────┘      └───────────────┘
+"""
+
+import subprocess
+
+from mininet.cli import CLI
+from mininet.link import TCLink
+from mininet.net import Mininet
+from mininet.node import OVSController
+from mininet.topo import Topo
+
+LEFT_HOST_NAME: str = 'left_h'
+MIDDLE_HOST_NAME: str = 'middle_h'
+RIGHT_HOST_NAME: str = 'right_h'
+
+
+class HostInTheMiddleTopo(Topo):
+
+    def build(self):
+
+        left_host = self.addHost(LEFT_HOST_NAME)
+        #middle_host = self.addSwitch("s1")
+        right_host = self.addHost(RIGHT_HOST_NAME)
+
+        #self.addLink(left_host, middle_host, bw=1000, delay="100ms")
+        #self.addLink(middle_host, right_host, bw=100, delay="100ms")
+        self.addLink(left_host, right_host, bw=100, delay="100ms")
+
+
+def get_interface(host: str, index: int):
+    return f"{host}-eth{index}"
+
+
+def disable_offload(host, offload: str, index: int):
+    host.cmd(f"ethtool -K {get_interface(host.name, index)} {offload} off")
+
+
+def drop_ip(host, index):
+    host.cmd(f"ip addr flush dev {get_interface(host.name, index)}")
+    host.cmd(f"ip -6 addr flush dev {get_interface(host.name, index)}")
+
+
+def run():
+    net = Mininet(topo=HostInTheMiddleTopo(),
+                  controller=OVSController, link=TCLink)
+
+    left_host = net[LEFT_HOST_NAME]
+    right_host = net[RIGHT_HOST_NAME]
+
+    left_host.cmd("tcpdump -w left.pcap &")
+    right_host.cmd("tcpdump -w right.pcap &")
+
+    left_host.cmd(f"yes | nc -l 0.0.0.0 8000 &")
+    left_host.cmd(f"sudo ./target/release/smol-speed > out.txt 2> err.txt &")
+
+    net.start()   
+    CLI(net)
+    net.stop()
+
+
+if __name__ == "__main__":
+    run()
+
---
+++ src/main.rs
@@ -0,0 +1,81 @@
+#![allow(clippy::collapsible_if)]
+
+use std::cmp;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::os::fd::AsRawFd;
+
+use smoltcp::iface::{Config, Interface, SocketSet};
+use smoltcp::phy::{wait as phy_wait, Device, Medium};
+use smoltcp::socket::tcp;
+use smoltcp::time::{Duration, Instant};
+use smoltcp::wire::{EthernetAddress, IpAddress, IpCidr};
+
+const AMOUNT: usize = 1_000_000_000;
+
+static CLIENT_DONE: AtomicBool = AtomicBool::new(false);
+
+fn main() {
+    let mut device = smoltcp::phy::RawSocket::new("left_h-eth0", Medium::Ethernet).unwrap();
+    let fd = device.as_raw_fd();
+
+    const BUF_SIZE: usize = 65535;
+
+    let tcp1_rx_buffer = tcp::SocketBuffer::new(vec![b'A'; BUF_SIZE]);
+    let tcp1_tx_buffer = tcp::SocketBuffer::new(vec![b'A'; BUF_SIZE]);
+    let tcp1_socket = tcp::Socket::new(tcp1_rx_buffer, tcp1_tx_buffer);
+
+    let mut config = match device.capabilities().medium {
+        Medium::Ethernet => {
+            Config::new(EthernetAddress([0xd0,0x57,0x7b,0x11,0xc9,0x05,]).into())
+        }
+        Medium::Ip => Config::new(smoltcp::wire::HardwareAddress::Ip),
+        Medium::Ieee802154 => todo!(),
+    };
+    config.random_seed = rand::random();
+
+    let mut iface = Interface::new(config, &mut device, Instant::now());
+    iface.update_ip_addrs(|ip_addrs| {
+        ip_addrs
+            .push(IpCidr::new(IpAddress::v4(10, 0, 0, 5), 24))
+            .unwrap();
+    });
+
+    let mut sockets = SocketSet::new(vec![]);
+    let tcp1_handle = sockets.add(tcp1_socket);
+    let default_timeout = Some(Duration::from_millis(1000));
+
+    let mut processed = 0;
+    while !CLIENT_DONE.load(Ordering::SeqCst) {
+        let timestamp = Instant::now();
+        iface.poll(timestamp, &mut device, &mut sockets);
+
+        // tcp:1234: emit data
+        let socket = sockets.get_mut::<tcp::Socket>(tcp1_handle);
+        if !socket.is_open() {
+            socket.listen(8000).unwrap();
+        }
+
+        if socket.can_send() {
+            if processed < AMOUNT {
+                let length = socket
+                    .send(|buffer| {
+                        let length = cmp::min(buffer.len(), AMOUNT - processed);
+                        (length, length)
+                    })
+                    .unwrap();
+                processed += length;
+                dbg!(processed);
+            }
+        }
+
+        match dbg!(iface.poll_at(timestamp, &sockets)) {
+            Some(poll_at) if timestamp < poll_at => {
+                phy_wait(fd, Some(poll_at - timestamp)).expect("wait error");
+            }
+            Some(_) => (),
+            None => {
+                phy_wait(fd, default_timeout).expect("wait error");
+            }
+        }
+    }
+}
